c4817affc08a80a74d89d498f2c363dc2dde628f
santosh <santosh.ece06@gmail.com>
2021-06-29 11:59:50 +0530

    hello react.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// JSX
+const App = () => <h1>Hello World JSX</h1>;
+
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
