b5ce0b9f5a7fb7c2a807747db74da2e198b5ca1a
santosh <santosh.ece06@gmail.com>
2021-06-29 12:34:17 +0530

    working with list and map.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import "./App.css";
 const App = () => {
   let counter = 100;
   let username = "Capgemini";
+  let list = ["Delhi", "Kolkata", "Mumbai", "Chennai", "KASHMIR", "raipur"];
 
   return (
     <div>
       <h1>Hello World</h1>
       <h1>{username}</h1>
       <h1>{counter}</h1>
+
+      <h1>Dynamic Web Site</h1>
+      {list.map((item) => (
+        <h3>{item}</h3>
+      ))}
+
+      <h1>Static Web Stite </h1>
+      <h3>Delhi</h3>
+      <h3>Kolkata</h3>
+      <h3>Mumbai</h3>
+      <h3>Chennai</h3>
     </div>
   );
 };
